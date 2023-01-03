aa4a30e82a7ed4de6796564f5e674bc5289567ec
camille chaize <c.chaize74@gmail.com>
2023-01-03 17:26:05 +0100

    MCOT update

--- Config/temperature_config.py
+++ Config/temperature_config.py
@@ -6,4 +6,4 @@ w = 2 * math.pi / period
 
 # This function is called on each time step to update outside temperature
 def update_outside_temperature(t: float) -> float:
-    return 273.15 + 24 + 8 * math.sin(w * t)
+    return 273.15 + 27 + 5 * math.sin(w * t)
